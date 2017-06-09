dbb5b6b6f16358470a719cc55bb70a518d08ab69
Aryan10 <aryan.shivam.10@gmail.com>
2017-06-09 07:20:39 +0530

    Create index.js

--- index.js
+++ index.js
@@ -26,9 +26,13 @@ description:tempmsg
 
 if (message.content.startsWith(prefix + 'eval')) {
 const clean = text => {
-if (typeof(text) === "string")
-return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203)); else 
+if (typeof(text) === "string") {
+return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
+}else {
 return text;
+}
+
+}
 try {
 const args = message.content.split(' ').slice(1);
 const code = args.join(' ');
