b404f847fec48dcc213e3b57a6e1cb6fab912627
Aryan10 <aryan.shivam.10@gmail.com>
2017-06-11 12:18:13 +0530

    Create index.js

--- index.js
+++ index.js
@@ -55,6 +55,7 @@ const code = args.join(' ');
 let evaled = eval(code);
 if (typeof evaled !== 'string')
 evaled = require('util').inspect(evaled);
+message.delete()
 message.channel.send({embed: {
 color:3447003,
 title:'EVAL',
