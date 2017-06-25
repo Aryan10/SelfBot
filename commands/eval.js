ad281dcaf866b4fed82f456a8e958b18e5f0051e
Aryan10 <aryan.shivam.10@gmail.com>
2017-06-25 22:17:27 +0530

    Update eval.js

--- commands/eval.js
+++ commands/eval.js
@@ -14,13 +14,14 @@ const code = args.join(' ');
 let evaled = eval(code);
 if (typeof evaled !== 'string')
 evaled = require('util').inspect(evaled);
-message.edit({embed: {
+   message.delete()
+message.channel.send({embed: {
 color:3447003,
 title:'EVAL',
 description:`**EVAL INPUT:**\n\`\`\`${code}\`\`\`\n\n**EVAL OUTPUT:**\n\`\`\`${clean(evaled)}\`\`\``
 }});
 } catch (err) {
-      message.edit({embed: {
+      message.channel.send({embed: {
       title:'EVAL ERROR',
       description:`**ERROR**\`\`\`xl\n${clean(err)}\n\`\`\``
       }});
