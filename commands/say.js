00cafe516733d9288e109e714435aed1be458e1b
Aryan10 <aryan.shivam.10@gmail.com>
2017-06-25 22:06:31 +0530

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -1,5 +1,6 @@
 exports.run = (client, message, args) => {
-
+  
+const Discord = require ("discord.js");
 const embed = new Discord.RichEmbed()
 .setColor(0x7EC0EE)
 .setDescription(message.content.split(" ").slice(1).join(" "))
