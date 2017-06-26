6570ba563d33fe52f2a515882ba5f83f0a92b72d
Aryan10 <aryan.shivam.10@gmail.com>
2017-06-26 08:20:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,56 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const prefix = "×";
+const pokegamer_prefix = "+";
+const ryan_prefix = "?";
+const dusk_prefix = "d-";
 client.login(process.env.TOKEN);
 
 client.on('message', message => {
 if (message.author.id !== client.user.id) return;
-if (!message.content.startsWith(prefix)) return;
 const args = message.content.split(" ");
-const command = args.shift().slice(prefix.length);  
+const command = args.shift().slice(prefix.length); 
+
+if (message.content.startsWith(prefix)){
 try {
 let commandFile = require(`./commands/${command}.js`);
 commandFile.run(client, message, args);
 } catch (err) {
 console.error(err);
 }
+}else
+if (message.content.startsWith(pokegamer_prefix)){
+try {
+let commandFile = require(`./commands/${command}.js`);
+commandFile.run(client, message, args);
+} catch (err) {
+console.error(err);
+}
+}else
+if (message.content.startsWith(ryan_prefix)){
+try {
+let commandFile = require(`./commands/${command}.js`);
+commandFile.run(client, message, args);
+} catch (err) {
+console.error(err);
+}
+}else
+if (message.content.startsWith(dusk_prefix)){
+try {
+let commandFile = require(`./commands/${command}.js`);
+commandFile.run(client, message, args);
+} catch (err) {
+console.error(err);
+}
+}else {
+return;
+}
 });
 
 client.on('guildMemberAdd', member => {
 if (member.guild.id !== '290162830009696257') return;
-if (client.user.id !== me) return;
-member.guild.defaultChannel.send('Welcome:smiley:');
+if (client.user.id !== "273865811133857792") return;
+member.guild.defaultChannel.send(`Welcome ${member}`);
 });
 
 process.on("unhandledRejection", err => {
